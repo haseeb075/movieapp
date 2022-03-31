24a823ce166a083056de4c42046100c1a014036e
haseeb075 <haseebiqbal000@gmail.com>
2022-03-31 10:36:55 +0500

    adding things for testing of affiliates and remaining test cases written to test assigned tokens

--- src/apis/movieApi.js
+++ src/apis/movieApi.js
@@ -1,3 +1,6 @@
 import axios from "axios";
 
-export default axios.create({url: "items" , baseURL: 'http://www.omdbapi.com/'})
+export default axios.create({
+  url: "items",
+  baseURL: "http://www.omdbapi.com/",
+});
