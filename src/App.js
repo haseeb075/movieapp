cbcf76194fb2d539659070e5766fca1067e812d6
haseeb075 <haseebiqbal000@gmail.com>
2022-04-05 10:21:59 +0500

    Add: Setup router , install sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/movie/:imdbID" element={<MovieDetail/>} />
+          <Route path="*" element={<PageNotFound/>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/apis/movieApi.js
+++ src/apis/movieApi.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   url: "items",
-  baseURL: "http://www.omdbapi.com/",
+  baseURL: "http://www.omdbapi.com",
 });
