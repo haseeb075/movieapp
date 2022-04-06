4b28c51be80eec82aa2d70c85e7962b978c8020e
haseeb075 <haseebiqbal000@gmail.com>
2022-04-06 09:04:59 +0500

    Add: Router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Logo} from '../../images/logo.svg'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <Link to="/">
+      
+      <div className='logo'>Movie App</div>
+      </Link>
+      <div className='user-image'>
+        <img src={Logo} alt="user"/>
+      </div>
+      
+    </div>
   )
 }
